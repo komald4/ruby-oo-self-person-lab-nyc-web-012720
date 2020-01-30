c00a4eca7d02e798d5b2f78ae7a23463bdbcec73
calm-fork-7535 <komald.4@gmail.com>
2020-01-30 21:04:56 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,24 @@
 # your code goes here
+class Person
+  attr_accessor :happiness, :bank_account
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    end
+
+  def happiness
+    if @happiness < 10
+      @happiness = 10
+    else @happiness < 0
+          @happiness = 0
+  else @happiness
+ end
+
+
+
+
+
+end
