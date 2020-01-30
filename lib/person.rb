f3a20a50b8c474e697b18e2eac5f38cb52e9cd51
calm-fork-7535 <komald.4@gmail.com>
2020-01-30 21:05:41 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,85 +1,85 @@
-class Person 
-  
-  
+class Person
+
+
   attr_accessor :happiness, :bank_account, :hygiene
   attr_reader :name
-  
+
   def initialize (name)
-    @name = name  
+    @name = name
     @bank_account = 25
     @happiness = 8
     @hygiene  = 8
   end
 
-  
+
   def happiness
-     if @happiness > 10 
-      @happiness = 10 
-    elsif @happiness < 0 
+     if @happiness > 10
+      @happiness = 10
+    elsif @happiness < 0
       @happiness = 0
     else
       @happiness
     end
   end
-  
+
   def hygiene=(num)
     @hygiene = num
   end
-  
+
   def hygiene
-     if @hygiene > 10 
-      @hygiene = 10 
-    elsif @hygiene < 0 
+     if @hygiene > 10
+      @hygiene = 10
+    elsif @hygiene < 0
       @hygiene = 0
     else
       @hygiene
     end
   end
-    
-    
+
+
   def happy?
     if @happiness > 7
-      true 
+      true
     else
-      false 
+      false
     end
   end
-    
+
   def clean?
     if @hygiene > 7
-      true 
+      true
     else
-      false 
+      false
     end
-  end  
+  end
 
 
   def get_paid(salary)
     @bank_account = @bank_account + salary
     return "all about the benjamins"
   end
-  
+
   def take_bath
     self.hygiene = @hygiene + 4
     return "♪ Rub-a-dub just relaxing in the tub ♫"
   end
-  
+
   def work_out
     self.happiness += 2
     self.hygiene -= 3
     return "♪ another one bites the dust ♫"
   end
-  
-  
+
+
   def call_friend(friend)
     friend.happiness += 3
     self.happiness += 3
     return "Hi #{friend.name}! It's #{self.name}. How are you?"
     end
-  
+
   def start_conversation (start, topic)
     if topic == "politics"
-      
+
       self.happiness -= 2
       start.happiness -= 2
       return "blah blah partisan blah lobbyist"
@@ -87,7 +87,7 @@ class Person
       self.happiness += 1
       start.happiness += 1
       return "blah blah sun blah rain"
-    else 
+    else
       return "blah blah blah blah blah"
     end
   end
